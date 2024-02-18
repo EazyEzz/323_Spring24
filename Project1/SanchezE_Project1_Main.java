611ad7b7b74150b4a72a9c53c508494567688cea
EazyEzz <essmer.sanchez99@qmail.cuny.edu>
2024-02-18 05:48:24 -0500

    Project1 complete

--- Project1/SanchezE_Project1_Main.java
+++ Project1/SanchezE_Project1_Main.java
@@ -16,13 +16,34 @@ public class SanchezE_Project1_Main{
         File outFile = new File(args[1]);
         File debugFile = new File(args[2]);
 
-        FileWriter debugWriter = new FileWriter(debugFile);
-        FileWriter writer = new FileWriter(outFile);
+        try(FileWriter debugWriter = new FileWriter(debugFile, true);
+            FileWriter outWriter = new FileWriter(outFile, true);){
+            debugWriter.write("In main(), after opening all files\n\n");
 
-        debugWriter.write("In main(), after open all files");
+            LLStack llStack = new LLStack();
+    
+            llStack.buildStack(inFile, debugWriter);
+    
+            outWriter.write("In main() after buildStack and printing stack\n");
+            outWriter.write(llStack.printStack() + "\n\n");
 
-        LLStack llStack = new LLStack();
+            LLQueue llQueue = new LLQueue();
 
+            llQueue.buildQueue(inFile, debugWriter);
+
+            outWriter.write("In main() after buildQueue and printing Queue\n");
+            outWriter.write(llQueue.printQueue() + "\n\n");
+
+            LLlist llList = new LLlist();
+
+            llList.buildList(inFile, debugWriter);
+
+            outWriter.write("In main() after buildList and printing List\n");
+            outWriter.write(llList.printList() + "\n\n");
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }        
     }
 }
 class listNode{
@@ -36,16 +57,21 @@ class listNode{
 
     //static allows for method access without creating an instance of class
     //print out format: (node data, node next data)
-    static void printNode(listNode node){
+    static String printNode(listNode node){
+        String s = "";
         if(node != null){ //does node exist?
             if(node.next != null){
-                System.out.println("(" + node.data + "," + node.next.data + ")");
+                //System.out.println("(" + node.data + "," + node.next.data + ")");
+                s = "(" + node.data + "," + node.next.data + ")";
             } else {
-                System.out.println("(" + node.data + ", null)");
+                //System.out.println("(" + node.data + ", null)");
+                s = "(" + node.data + ", null)";
             }
         } else {
-            System.out.println("(null,null)");
+            //System.out.println("(null,null)");
+            s = "(null,null)";
         }
+        return s;
     }
 }
 
@@ -56,11 +82,10 @@ class LLStack{
 
     LLStack(){
         this.top = new listNode(-9999);
-        //new listNode default = null
+        //new listNode next default = null
     }
 
-    void push(int data){
-        listNode newNode = new listNode(data);
+    void push(listNode newNode){
         newNode.next = top.next;
         top.next = newNode;
     }
@@ -69,22 +94,234 @@ class LLStack{
         return top.next == null;
     }
 
-    void pop(){
-        if (isEmpty()){
-            throw new RuntimeException("Stack is Empty");
-        }else {
-            listNode popNode = top.next;
-            top.next = popNode.next;
+    listNode pop(){
+        listNode t = top;
+        if (t.next != null){
+            listNode tmp = t.next;
+            t.next = tmp.next;
+            tmp.next = null;
+            return tmp;
+        } else {
+            return null;
         }
     }
 
-    void buildStack(LLStack currStack, File inFile, File debugFile){
-    try(
-        Scanner scanner = new Scanner(new FileReader(inFile));
-        FileWriter debugWriter = new FileWriter(debugFile)){
-
-        } catch (IOException e) {
-                e.printStackTrace();
+    String printStack(){
+        StringBuilder sb = new StringBuilder("Top");
+        listNode currentNode = top;
+        while(currentNode != null){
+            sb.append(" -> ").append(listNode.printNode(currentNode));
+            currentNode = currentNode.next;
         }
+        sb.append(" -> NULL");
+        return sb.toString();
+    }
+
+    void buildStack(File inFile, FileWriter debugWriter){
+        try (
+            Scanner scanner = new Scanner(new FileReader(inFile));) {
+
+            debugWriter.write("Entering buildStack()\n");
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (!line.isEmpty()) {
+                    char op = line.charAt(0);
+                    int data = Integer.parseInt(line.substring(1).trim());
+
+                    debugWriter.write("In buildStack(): op = " + op + ", data = " + data + "\n");
+
+                    if (op == '+') {
+                        listNode newNode = new listNode(data);
+                        push(newNode);
+                    } else if (op == '-') {
+                        listNode topNode = pop();
+                        if (topNode != null) {
+                            debugWriter.write("In buildStack(), top node deleted\n");
+                            topNode = null;
+                        } else {
+                            debugWriter.write("In buildStack(), stack is empty\n");
+                        }
+                    } else {
+                        debugWriter.write("In buildStack(), op = " + op + " is illegal\n");
+                    }
+
+                }
+            }
+            debugWriter.write("In buildStack(), printing Stack\n");
+            debugWriter.write(printStack() + "\n");
+            debugWriter.write("Leaving buildStack()\n\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class LLQueue{
+    private listNode head;
+    private listNode tail;
+
+    LLQueue(){
+        this.head = new listNode(-9999);
+        this.tail = head;
+    }
+
+    void insertQ(listNode newNode) {
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    listNode deleteQ(){
+        listNode t = head;
+        if(t.next != null) {
+            if(tail == t.next){
+                tail = head;
+            }
+            listNode tmp = t.next;
+            t.next = tmp.next;
+            tmp.next = null;
+            return tmp;
+        }
+        return null;
+    }
+
+    Boolean isEmpty(){
+        return tail == head;
+    }
+
+    String printQueue(){
+        StringBuilder sb = new StringBuilder("Head");
+        listNode currentNode = head;
+        while(currentNode != null){
+            sb.append(" -> ").append(listNode.printNode(currentNode));
+            currentNode = currentNode.next;
+        }
+        sb.append(" -> NULL");
+        return sb.toString();
+    }
+
+    void buildQueue(File inFile, FileWriter debugWriter){
+        try(
+            Scanner scanner = new Scanner(new FileReader(inFile));
+            ){
+                debugWriter.write("Entering buildQueue()\n");
+    
+                while (scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    if (!line.isEmpty()) {
+                        char op = line.charAt(0);
+                        int data = Integer.parseInt(line.substring(1).trim());
+    
+                        debugWriter.write("In buildQueue(): op = " + op + ", data = " + data + "\n");
+    
+                        if (op == '+') {
+                            listNode newNode = new listNode(data);
+                            insertQ(newNode);
+                        } else if (op == '-') {
+                            listNode tailNode = deleteQ();
+                            if (tailNode != null) {
+                                debugWriter.write("In buildQueue(), tailNode's data is " + tailNode.data + "\n");
+                                tailNode = null;
+                            } else {
+                                debugWriter.write("In buildQueue(), the Queue is empty\n");
+                            }
+                        } else {
+                            debugWriter.write("In buildQueue(), op = " + op + " is illegal\n");
+                        }
+    
+                    }
+                }
+                debugWriter.write("In buildQueue(), printing Queue\n");
+                debugWriter.write(printQueue() + "\n");
+                debugWriter.write("Leaving buildQueue()\n\n");            
+            } catch (IOException e) {
+                    e.printStackTrace();
+            }
+    }
+}
+
+class LLlist{
+    private listNode listHead;
+
+    LLlist(){
+        this.listHead = new listNode(-9999);
+    }
+
+    listNode findSpot(listNode newNode){
+        listNode spot = listHead;
+        if(listHead.next != null && newNode.data > spot.next.data){
+            spot = spot.next;
+        }
+        return spot;
+    }
+
+    void insertOneNode(listNode spot, listNode newNode){
+        newNode.next = spot.next;
+        spot.next = newNode;
+    }
+
+    listNode deleteOneNode(int data){
+        listNode spot = listHead;
+        while(spot.next != null && spot.next.data < data){
+            spot = spot.next;
+        }
+        if(spot.next != null && spot.next.data == data){
+            listNode tmp = spot.next;
+            spot.next = tmp.next;
+            tmp.next = null;
+            return tmp;
+        }
+        return null;
+    }
+
+    String printList(){
+        StringBuilder sb = new StringBuilder("listHead");
+        listNode currentNode = listHead;
+        while(currentNode != null){
+            sb.append(" -> ").append(listNode.printNode(currentNode));
+            currentNode = currentNode.next;
+        }
+        sb.append(" -> NULL");
+        return sb.toString();
+    }
+
+    void buildList(File inFile, FileWriter debugWriter){
+        try(
+            Scanner scanner = new Scanner(new FileReader(inFile));
+            ){
+                debugWriter.write("Entering buildList()\n");
+    
+                while (scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    if (!line.isEmpty()) {
+                        char op = line.charAt(0);
+                        int data = Integer.parseInt(line.substring(1).trim());
+    
+                        debugWriter.write("In buildList(): op = " + op + ", data = " + data + "\n");
+    
+                        if (op == '+') {
+                            listNode newNode = new listNode(data);
+                            listNode spot = findSpot(newNode);
+                            insertOneNode(spot, newNode);
+                        } else if (op == '-') {
+                            listNode junk = deleteOneNode(data);
+                            if (junk != null) {
+                                debugWriter.write("In buildList(), data found and node deleted\n");
+                                junk = null;
+                            } else {
+                                debugWriter.write("In buildList(), data is not in the list\n");
+                            }
+                        } else {
+                            debugWriter.write("In buildList(), op = " + op + " is illegal\n");
+                        }
+    
+                    }
+                }
+                debugWriter.write("In buildList(), printing List\n");
+                debugWriter.write(printList() + "\n");
+                debugWriter.write("Leaving buildList()\n\n");            
+            } catch (IOException e) {
+                    e.printStackTrace();
+            }
     }
 }
